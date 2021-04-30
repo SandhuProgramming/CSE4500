4a55988c8bb5376736657a70612b9ba7bd18f2fb
unknown <Tj.Sandhu17@hotamil.com>
2021-04-29 17:39:17 -0700

    Progress Report 7

--- src/App.js
+++ src/App.js
@@ -28,6 +28,56 @@ import AnimalShapes from "./components/AnimalShapes";
 import AnimateDead from "./components/AnimateDead";
 import AnimateObjects from "./components/AnimateObjects";
 import AntilifeShell from "./components/AntilifeShell";
+import AntimagicField from "./components/AntimagicField";
+import AntipathySympathy from "./components/AntipathySympathy";
+import ArcaneEye from "./components/ArcaneEye";
+import ArcaneHand from "./components/ArcaneHand";
+import ArcaneLock from "./components/ArcaneLock";
+import ArcaneSword from "./components/ArcaneSword";
+import ArcanistsMagicAura from "./components/ArcanistsMagicAura";
+import AstralProjection from "./components/AstralProjection";
+import Augury from "./components/Augury";
+import Awaken from "./components/Awaken";
+import Bane from "./components/Bane";
+import Banishment from "./components/Banishment";
+import Barkskin from "./components/Barkskin";
+import BeaconOfHope from "./components/BeaconOfHope";
+import BestowCurse from "./components/BestowCurse";
+import BlackTentacles from "./components/BlackTentacles";
+import BladeBarrier from "./components/BladeBarrier";
+import Bless from "./components/Bless";
+import Blight from "./components/Blight";
+import BlindnessDeafness from "./components/BlindnessDeafness";
+import Blink from "./components/Blink";
+import Blur from "./components/Blur";
+import BrandingSmite from "./components/BrandingSmite";
+import BurningHands from "./components/BurningHands";
+import CallLightning from "./components/CallLightning";
+import CalmEmotions from "./components/CalmEmotions";
+import ChainLightning from "./components/ChainLightning";
+import CharmPerson from "./components/CharmPerson";
+import ChillTouch from "./components/ChillTouch";
+import CircleOfDeath from "./components/CircleOfDeath";
+import Clairvoyance from "./components/Clairvoyance";
+import Clone from "./components/Clone";
+import Cloudkill from "./components/Cloudkill";
+import ColorSpray from "./components/ColorSpray";
+import Command from "./components/Command";
+import Commune from "./components/Commune";
+import CommuneWithNature from "./components/CommuneWithNature";
+import ComprehendLanguages from "./components/ComprehendLanguages";
+import Compulsion from "./components/Compulsion";
+import ConeOfCold from "./components/ConeOfCold";
+import Confusion from "./components/Confusion";
+import ConjureAnimals from "./components/ConjureAnimals";
+import ConjureCelestial from "./components/ConjureCelesial";
+import ConjureElemental from "./components/ConjureElemental";
+import ConjureFey from "./components/ConjureFey";
+import ConjureMinorElementals from "./components/ConjureMinorElementals";
+import ConjureWoodlandBeings from "./components/ConjureWoodlandBeings";
+import ContactOtherPlane from "./components/ContactOtherPlane";
+import Contagion from "./components/Contagion";
+import Contingency from "./components/Contingency";
 
 function App() {
   return (
@@ -111,6 +161,156 @@ function App() {
               </Route>
               <Route path="/antilife-shell">
                 <AntilifeShell />
+              </Route>
+              <Route path="/antimagic-field">
+                <AntimagicField />
+              </Route>
+              <Route path="/antipathy-sympathy">
+                <AntipathySympathy />
+              </Route>
+              <Route path="/arcane-eye">
+                <ArcaneEye />
+              </Route>
+              <Route path="/arcane-hand">
+                <ArcaneHand />
+              </Route>
+              <Route path="/arcane-lock">
+                <ArcaneLock />
+              </Route>
+              <Route path="/arcane-sword">
+                <ArcaneSword />
+              </Route>
+              <Route path="/arcanists-magic-aura">
+                <ArcanistsMagicAura />
+              </Route>
+              <Route path="/astral-projection">
+                <AstralProjection />
+              </Route>
+              <Route path="/augury">
+                <Augury />
+              </Route>
+              <Route path="/awaken">
+                <Awaken />
+              </Route>
+              <Route path="/bane">
+                <Bane />
+              </Route>
+              <Route path="/banishment">
+                <Banishment />
+              </Route>
+              <Route path="/barkskin">
+                <Barkskin />
+              </Route>
+              <Route path="/beacon-of-hope">
+                <BeaconOfHope />
+              </Route>
+              <Route path="/bestow-curse">
+                <BestowCurse />
+              </Route>
+              <Route path="/black-tentacles">
+                <BlackTentacles />
+              </Route>
+              <Route path="/blade-barrier">
+                <BladeBarrier />
+              </Route>
+              <Route path="/bless">
+                <Bless />
+              </Route>
+              <Route path="/blight">
+                <Blight />
+              </Route>
+              <Route path="/blindness-deafness">
+                <BlindnessDeafness />
+              </Route>
+              <Route path="/blink">
+                <Blink />
+              </Route>
+              <Route path="/blur">
+                <Blur />
+              </Route>
+              <Route path="/branding-smite">
+                <BrandingSmite />
+              </Route>
+              <Route path="/burning-hands">
+                <BurningHands />
+              </Route>
+              <Route path="/call-lightning">
+                <CallLightning />
+              </Route>
+              <Route path="/calm-emotions">
+                <CalmEmotions />
+              </Route>
+              <Route path="/chain-lightning">
+                <ChainLightning />
+              </Route>
+              <Route path="/charm-person">
+                <CharmPerson />
+              </Route>
+              <Route path="/chill-touch">
+                <ChillTouch />
+              </Route>
+              <Route path="/circle-of-death">
+                <CircleOfDeath />
+              </Route>
+              <Route path="/clairvoyance">
+                <Clairvoyance />
+              </Route>
+              <Route path="/clone">
+                <Clone />
+              </Route>
+              <Route path="/cloudkill">
+                <Cloudkill />
+              </Route>
+              <Route path="/color-spray">
+                <ColorSpray />
+              </Route>
+              <Route path="/command">
+                <Command />
+              </Route>
+              <Route path="/commune">
+                <Commune />
+              </Route>
+              <Route path="/commune-with-nature">
+                <CommuneWithNature />
+              </Route>
+              <Route path="/comprehend-languages">
+                <ComprehendLanguages />
+              </Route>
+              <Route path="/compulsion">
+                <Compulsion />
+              </Route>
+              <Route path="/cone-of-cold">
+                <ConeOfCold />
+              </Route>
+              <Route path="/confusion">
+                <Confusion />
+              </Route>
+              <Route path="/conjure-animals">
+                <ConjureAnimals />
+              </Route>
+              <Route path="/conjure-celestial">
+                <ConjureCelestial />
+              </Route>
+              <Route path="/conjure-elemental">
+                <ConjureElemental />
+              </Route>
+              <Route path="/conjure-fey">
+                <ConjureFey />
+              </Route>
+              <Route path="/conjure-minor-elementals">
+                <ConjureMinorElementals />
+              </Route>
+              <Route path="/conjure-woodland-beings">
+                <ConjureWoodlandBeings />
+              </Route>
+              <Route path="/contact-other-plane">
+                <ContactOtherPlane />
+              </Route>
+              <Route path="/contagion">
+                <Contagion />
+              </Route>
+              <Route path="/contingency">
+                <Contingency />
               </Route>
             </Switch>
           </BrowserRouter>
